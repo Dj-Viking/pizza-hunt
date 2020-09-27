7d0f173fea3112cd3c20896f8ce1dbbed29f1f5b
DJ Viking <anders.swedishviking@gmail.com>
2020-09-26 20:39:22 -0400

    added comment model and the vitual for the pizza to reference comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -17,9 +17,30 @@ const PizzaSchema = new Schema
       type: String,
       default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [//creating relationship here between comment and pizza
+      //expect an objectId and the data is coming from comment model
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {//to use virtuals tell the schema to use virtuals
+    toJSON: {
+      virtuals: true,
+    },
+    id: false//setting this false since this is a virtual that mongoose returns
   }
 );
+
+//creating pizza virtual to add more information to the database
+// response so that we dont have to add the information manually
+// with a helper before responding to the API request
+PizzaSchema.virtual('commentCount')
+.get(function() {
+  return this.comments.length; 
+});
 
 const Pizza = model('Pizza', PizzaSchema);
 
