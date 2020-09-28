e5de4f4bea59be19cd6af3c1064e0eadc5d87613
DJ Viking <anders.swedishviking@gmail.com>
2020-09-28 11:45:28 -0400

    created reply methods

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,6 +3,9 @@ const { Comment, Pizza } = require('../models');
 const commentController = {
   //add comment to pizza
   addComment({ params, body }, res) {//req.params, req.body
+    console.log(``);
+    console.log("\x1b[33m", "client request to add a comment", "\x1b[00m");
+    console.log(``);
     console.log(body);
     Comment.create(body)
     .then(({ _id }) => {//body._id
@@ -15,6 +18,7 @@ const commentController = {
       );
     })
     .then(dbPizzaData => {
+      console.log(dbPizzaData);
       if (!dbPizzaData) {
         res.status(404).json({message: `no pizza found with the id of ${params.pizzaId}`});
         return;
@@ -25,6 +29,10 @@ const commentController = {
   },
   //delete comment
   delComment({ params }, res) {
+    console.log(``);
+    console.log("\x1b[33m", "client request to delete a comment", "\x1b[00m");
+    console.log(``);
+    console.log(params);
     Comment.findOneAndDelete
     (
       { _id: params.commentId }
@@ -42,6 +50,7 @@ const commentController = {
       );
     })
     .then(dbPizzaData => {
+      console.log(dbPizzaData);
       if (!dbPizzaData) {
         res.status(404).json({ message: `no pizza with the id of ${params.pizzaId}` });
         return;
@@ -49,7 +58,58 @@ const commentController = {
       res.status(200).json(dbPizzaData);
     })
     .catch(e => { console.log(e); res.json(e); });
+  },
+  //add reply to a comment
+  addReply: async ({ params, body }, res) => {
+    console.log(``);
+    console.log("\x1b[33m", "client request to add a reply to a comment", "\x1b[00m");
+    console.log(``);
+    console.log(params);
+    console.log(body);
+    try {
+      const dbPizzaData = await Comment.findOneAndUpdate
+      (
+        { _id: params.commentId },
+        { $push: { replies: body } },
+        { new: true }
+      );
+      console.log(dbPizzaData);
+      if (!dbPizzaData) {
+        res.status(404).json({ message: `no comment? or pizza? found with the id of ${params.commentId}`});
+        return;
+      } else {
+        res.status(200).json(dbPizzaData);
+      }
+    } catch (error) {
+      console.log(error);
+      res.json(error);
+    }
+  },
+  //delete a reply to a comment
+  delReply: async ({ params }, res) => {
+    console.log(``);
+    console.log("\x1b[33m", "client request to delete a reply on a comment", "\x1b[00m");
+    console.log(``);
+    console.log(params);
+    try {
+      const dbPizzaData = await Comment.findOneAndUpdate
+      (
+        { _id: params.commentId },
+        { $pull: { replies: { replyId: params.replyId } } },
+        { new: true }
+      );
+      console.log(dbPizzaData);
+      if (!dbPizzaData) {
+        res.status(404).json({ message: `no pizza or comment found with the id of ${params.commentId} or reply id ${params.replyId}`});
+        return;
+      } else {
+        res.status(200).json(dbPizzaData);
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
   }
-}
+};
 
 module.exports = commentController;
